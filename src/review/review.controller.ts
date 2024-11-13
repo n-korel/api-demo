ccdc6325e1d37ac45a6750ecee2f0d8db0f4a5b1
n-korel <n.korelskiy@hotmail.com>
2024-11-13 17:53:27 +0300

    Авторизация

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -14,7 +14,7 @@ import {
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
-import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('review')
 export class ReviewController {
